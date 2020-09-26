77566137126ae881715969d89f94008d8c1ea9b9
Silas Stoffel <silasstofel@gmail.com>
2020-09-26 12:11:56 -0300

    firt commi

---
+++ src/Service/Assistidor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\Solid\Service;
+
+use Alura\Solid\Model\Pontuavel;
+
+class Assistidor
+{
+
+    public function assistir(Pontuavel $conteudo)
+    {
+        $conteudo->assistir();
+    }
+}
---
+++ src/Service/CalculadorPontuacao.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alura\Solid\Service;
+
+use Alura\Solid\Model\AluraMais;
+use Alura\Solid\Model\Curso;
+use Alura\Solid\Model\Pontuavel;
+
+class CalculadorPontuacao
+{
+    public function recuperarPontuacao(Pontuavel $conteudo)
+    {
+        return $conteudo->recuperarPontuacao();
+    }
+}
